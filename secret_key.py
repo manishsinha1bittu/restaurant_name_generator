d22cd451659a0c1ef86e85f7c04268dd0639f0c4
Manish Sinha <manishsinha1bittu@gmail.com>
2024-01-11 18:31:58 +0530

    Update secret_key.py
    Added comments

--- secret_key.py
+++ secret_key.py
@@ -1,1 +1,3 @@
 api_key = "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"
+
+##This is an expired key. Please update this with your key and make the repository private otherwise OPENAI will treat the key as compromised and make it disabled
